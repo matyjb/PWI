323247bd9797ecd85917bee243023e7000f63123
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2019-03-17 13:52:37 +0100

    working reset

--- kolkoikrzyzyk/game.js
+++ kolkoikrzyzyk/game.js
@@ -1,30 +1,40 @@
 var player=true;
+var emptyChar=String.fromCharCode(160); //twarda spacja
 
 function handleClick(id) {
-  if(player) placeOn(id);
+  if(player) handlePlayerMove(id);
 }
 
-function handleBotsMove() {
-  var emptyFields = Array.from(document.getElementsByClassName("field")).filter(element => element.innerText=="_");
+function handleBotMove() {
+  var emptyFields = Array.from(document.getElementsByClassName("field")).filter(element => element.innerText==emptyChar);
   if(emptyFields.length>0) {
     id = Math.floor(Math.random() * emptyFields.length);
-    emptyFields[id].innerText = player ? "O" : "X";
-    player=!player;
+    if(!player){ //still bots move
+      emptyFields[id].innerText = player ? "O" : "X";
+      player=!player;
+    }
   }
 }
 
-function placeOn(id) {
-  document.getElementsByClassName("nr"+id)[0].innerText = player ? "O" : "X";
-  player = !player;
+function handlePlayerMove(id) {
+  if(document.getElementsByClassName("nr"+id)[0].innerText == emptyChar){
+    document.getElementsByClassName("nr"+id)[0].innerText = player ? "O" : "X";
+    player = !player;
+  }
   if(!player) { //run bot's move
-  setTimeout(handleBotsMove, Math.floor(Math.random() * 2000) + 100);
+  setTimeout(handleBotMove, Math.floor(Math.random() * 2000) + 100);
   }
 }
 
 function clearBoard() {
   Array.from(document.getElementsByClassName("field")).forEach((element,i)=>{
-    element.innerText="_";
+    element.innerText=emptyChar;
   })
+}
+
+function resetGame() {
+  player = true;
+  clearBoard();
 }
 
 function bodyOnLoad() {
