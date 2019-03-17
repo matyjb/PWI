3c6b798adf12172ba4babad4b5e4903c2d849bc8
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2019-03-17 18:50:19 +0100

    whose turn indicator

--- kolkoikrzyzyk/game.js
+++ kolkoikrzyzyk/game.js
@@ -1,7 +1,27 @@
-var player=true;
+var player;
 var emptyChar=String.fromCharCode(160); //twarda spacja
 var O = "<img src='./../assets/O.png'></img>";
 var X = "<img src='./../assets/X.png'></img>";
+
+function playerChange(p) {
+  if(p!=undefined){
+    player = p;
+  }else{
+    player=!player;
+  }
+  Array.from(document.getElementsByClassName("turn")).forEach((element)=>{
+    switch (element.dataset.who) {
+      case "player":
+        element.style.backgroundColor = player ? "#039be5" : "inherit";
+        element.style.color = player ? "white" : "black";
+        break;
+      case "bot":
+        element.style.backgroundColor = player ? "inherit" : "#039be5";
+        element.style.color = player ? "black" : "white";
+        break;
+    }
+  })
+}
 
 function handleClick(id) {
   if(player) handlePlayerMove(id);
@@ -13,16 +33,15 @@ function handleBotMove() {
     id = Math.floor(Math.random() * emptyFields.length);
     if(!player){ //still bots move
       emptyFields[id].innerHTML = player ? O : X;
-      player=!player;
+      playerChange();
     }
   }
 }
 
 function handlePlayerMove(id) {
   if(document.getElementsByClassName("nr"+id)[0].innerText == emptyChar){
-    // document.getElementsByClassName("nr"+id)[0].innerText = player ? "O" : "X";
     document.getElementsByClassName("nr"+id)[0].innerHTML = player ? O : X;
-    player = !player;
+    playerChange();
   }
   if(!player) { //run bot's move
   setTimeout(handleBotMove, Math.floor(Math.random() * 2000) + 100);
@@ -36,10 +55,11 @@ function clearBoard() {
 }
 
 function resetGame() {
-  player = true;
+  playerChange(true);
   clearBoard();
 }
 
 function bodyOnLoad() {
   clearBoard();
+  playerChange(true);
 }
